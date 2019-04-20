24d15cdeb6bc46974d4e759de6905a10e2fbaf19
Thomas Trinca <tptrinca@gmail.com>
2019-04-20 12:00:04 +1000

    Add FABs subtitle

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>This app has been configured to output FABs</p>
   </div>
 </template>
 
